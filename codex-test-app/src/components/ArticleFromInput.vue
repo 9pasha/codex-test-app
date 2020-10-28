cbeda5574e931c5e2a34cbbb433ca5705f464983
Pavel Bolozhko <pavel.b820@gmail.com>
2020-10-28 14:45:16 +0300

    feat: implement view data from input.txt

--- codex-test-app/src/components/ArticleFromInput.vue
+++ codex-test-app/src/components/ArticleFromInput.vue
@@ -1,6 +1,9 @@
 <template>
   <v-container>
-    {{ text }}
+    <p>Canvas size: <span class="indigo--text">{{ canvasSize }}</span></p>
+    <p>Line: <span class="indigo--text">{{ line }}</span></p>
+    <p>Rectangle: <span class="indigo--text">{{ rectangle }}</span></p>
+    <p>Bucket fill: <span class="indigo--text">{{ fillBucket }}</span></p>
   </v-container>
 </template>
 
@@ -11,8 +14,24 @@ export default {
   data: () => ({
     text: Input,
   }),
-  methods: {
-  },
+  computed: {
+    canvasSize() {
+      const reg = /C \d+ \d+/gi;
+      return this.text.match(reg).join("; ");
+    },
+    line() {
+      const reg = /L \d+ \d+ \d+ \d+/gi;
+      return this.text.match(reg).join("; ");
+    },
+    rectangle() {
+      const reg = /R \d+ \d+ \d+ \d+/gi;
+      return this.text.match(reg).join("; ");
+    },
+    fillBucket() {
+      const reg = /B \d+ \d+ \w/gi;
+      return this.text.match(reg).join("; ");
+    }
+  }
 }
 </script>
 
