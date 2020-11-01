14d1a3cfe4387cf57920fcc4ce043e32b2f4e8f8
Pavel Bolozhko <pavel.b820@gmail.com>
2020-11-01 19:03:00 +0300

    feat: add test cases for Canvas component

--- codex-test-app/src/components/Canvas.vue
+++ codex-test-app/src/components/Canvas.vue
@@ -11,7 +11,6 @@
         </div>
       </div>
     </div>
-
     <h2 v-if="canvasErr" class="red--text text-center">
       {{ canvasErr }}
     </h2>
--- codex-test-app/tests/unit/canvas.spec.js
+++ codex-test-app/tests/unit/canvas.spec.js
@@ -1,5 +1,6 @@
 import { shallowMount, createLocalVue } from "@vue/test-utils";
 import Canvas from "@/components/Canvas.vue";
+import CanvasPixel from "@/components/CanvasPixel";
 import Vuex from "vuex";
 
 const localVue = createLocalVue();
@@ -13,21 +14,37 @@ describe("Canvas.vue", () => {
     store = new Vuex.Store({
       state: {
         canvas: {
-          x: 20,
-          y: 20
-        }
+          x: 3,
+          y: 10
+        },
+        isCorrectCanvas: false
       }
     });
   });
-  it("check correct color ''", () => {
+  it("check error alert", () => {
+    const wrapper = shallowMount(Canvas, {
+      localVue,
+      store
+    });
+    const errorMsg =
+      "Sorry, canvas can't be created. At first line of input.txt you need to create canvas(C x y) and set min-size: 2x2.";
+
+    expect(wrapper.is(Canvas)).toBeTruthy();
+    expect(wrapper.contains(CanvasPixel)).toBe(false);
+    const errorEl = wrapper.find("h2");
+    expect(errorEl.text()).toEqual(errorMsg);
+  });
+  it("check render without error", () => {
+    store.state.isCorrectCanvas = true;
     const wrapper = shallowMount(Canvas, {
       localVue,
       store
     });
 
     expect(wrapper.is(Canvas)).toBeTruthy();
-    expect(wrapper.vm.x).toEqual(1);
-    expect(wrapper.vm.y).toEqual(1);
-    expect(wrapper.text()).toMatch("");
+    expect(wrapper.findComponent(CanvasPixel)).toBeTruthy();
+    const pixels = wrapper.findAllComponents(CanvasPixel);
+    expect(pixels).toHaveLength(30);
+    expect(wrapper.contains(".canvas h2")).toBe(false);
   });
 });
