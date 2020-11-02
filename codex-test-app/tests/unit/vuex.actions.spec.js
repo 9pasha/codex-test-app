7e9533323d6d80e36384bd3fdc52237e41696301
Pavel Bolozhko <pavel.b820@gmail.com>
2020-11-02 12:36:29 +0300

    feat: add tests for fillBucket and drawRect tools

---
+++ codex-test-app/tests/unit/vuex.actions.spec.js
@@ -0,0 +1,18 @@
+import { actions } from "@/store";
+
+const { createTextCanvas } = actions;
+
+// describe("actions", () => {
+//   it("test creating text copy of canvas", () => {
+//     const state = { pixels: [], canvas: { x: 5, y: 5 } };
+//
+//     createTextCanvas(state, { color: "" });
+//     expect(state.pixels).toEqual([
+//       ["", "", "", "", ""],
+//       ["", "", "", "", ""],
+//       ["", "", "", "", ""],
+//       ["", "", "", "", ""],
+//       ["", "", "", "", ""]
+//     ]);
+//   });
+// });
--- codex-test-app/tests/unit/vuex.mutations.spec.js
+++ codex-test-app/tests/unit/vuex.mutations.spec.js
@@ -56,43 +56,66 @@ describe("mutations", () => {
       ["o", "x", "o", "o", "o"]
     ]);
   });
-  // it("check draw rect function", () => {
-  //   const state = {
-  //     pixels: [],
-  //     canvas: { x: 5, y: 5 },
-  //     line: [{ x1: 2, y1: 2, x2: 2, y2: 5 }]
-  //   };
-  //
-  //   createCanvas(state, { color: "o" });
-  //   drawLine(state);
-  //   expect(state.pixels).toEqual([
-  //     ["o", "o", "o", "o", "o"],
-  //     ["o", "x", "o", "o", "o"],
-  //     ["o", "x", "o", "o", "o"],
-  //     ["o", "x", "o", "o", "o"],
-  //     ["o", "x", "o", "o", "o"]
-  //   ]);
-  // });
-  // it("check fill canvas function", () => {
-  //   const state = {
-  //     pixels: [],
-  //     canvas: { x: 5, y: 5 },
-  //     line: [
-  //       { x1: 2, y1: 2, x2: 2, y2: 5 },
-  //       { x1: 1, y1: 2, x2: 4, y2: 2 }
-  //     ]
-  //   };
-  //
-  //   createCanvas(state, { color: "o" });
-  //   drawLine(state);
-  //   drawLine(state);
-  //   fillCanvas(state, { x: 1, y: 4, color: "", currPixelColor: "o" });
-  //   expect(state.pixels).toEqual([
-  //     ["o", "o", "o", "o", "o"],
-  //     ["x", "x", "x", "x", "o"],
-  //     ["", "x", "o", "o", "o"],
-  //     ["", "x", "o", "o", "o"],
-  //     ["", "x", "o", "o", "o"]
-  //   ]);
-  // });
+  it("check draw rect function", () => {
+    const state = {
+      pixels: [],
+      canvas: { x: 5, y: 5 },
+      line: [],
+      rectangle: [
+        { x1: 1, y1: 1, x2: 3, y2: 3 },
+        { x1: 4, y1: 4, x2: 5, y2: 5 }
+      ]
+    };
+    const thisObj = {
+      commit: function() {
+        drawLine(state);
+      }
+    };
+
+    createCanvas(state, { color: "o" });
+    drawRect.call(thisObj, state);
+    drawRect.call(thisObj, state);
+    expect(state.pixels).toEqual([
+      ["x", "x", "x", "o", "o"],
+      ["x", "o", "x", "o", "o"],
+      ["x", "x", "x", "o", "o"],
+      ["o", "o", "o", "x", "x"],
+      ["o", "o", "o", "x", "x"]
+    ]);
+  });
+  it("check fill canvas function", () => {
+    const state = {
+      pixels: [],
+      canvas: { x: 5, y: 5 },
+      line: [],
+      rectangle: [{ x1: 1, y1: 1, x2: 3, y2: 3 }],
+      fillBucket: []
+    };
+    const thisObj = {
+      commit: function() {
+        drawLine(state);
+      }
+    };
+    const fillCanvasObj = {
+      commit: function(name, value) {
+        fillCanvas.call(fillCanvasObj, state, value);
+      }
+    };
+
+    createCanvas(state, { color: "o" });
+    drawRect.call(thisObj, state);
+    fillCanvas.call(fillCanvasObj, state, {
+      x: 1,
+      y: 1,
+      color: "",
+      currPixelColor: "x"
+    });
+    expect(state.pixels).toEqual([
+      ["", "", "", "o", "o"],
+      ["", "o", "", "o", "o"],
+      ["", "", "", "o", "o"],
+      ["o", "o", "o", "o", "o"],
+      ["o", "o", "o", "o", "o"]
+    ]);
+  });
 });
