83a9cc5737f0beeebfe4b68ce65b2f61523137c0
Pavel Bolozhko <pavel.b820@gmail.com>
2020-11-02 12:58:58 +0300

    feat: add vuex action tests

--- codex-test-app/tests/unit/vuex.actions.spec.js
+++ codex-test-app/tests/unit/vuex.actions.spec.js
@@ -1,18 +1,176 @@
-import { actions } from "@/store";
+import { actions, mutations } from "@/store";
 
 const { createTextCanvas } = actions;
+const { updateOutputText } = mutations;
 
-// describe("actions", () => {
-//   it("test creating text copy of canvas", () => {
-//     const state = { pixels: [], canvas: { x: 5, y: 5 } };
-//
-//     createTextCanvas(state, { color: "" });
-//     expect(state.pixels).toEqual([
-//       ["", "", "", "", ""],
-//       ["", "", "", "", ""],
-//       ["", "", "", "", ""],
-//       ["", "", "", "", ""],
-//       ["", "", "", "", ""]
-//     ]);
-//   });
-// });
+describe("actions", () => {
+  it("test 1 creating text copy of canvas", () => {
+    const state = {
+      pixels: [
+        ["x", "x", "x", "x", "x"],
+        ["x", "x", "x", "x", "x"],
+        ["x", "x", "x", "x", "x"],
+        ["x", "x", "x", "x", "x"],
+        ["x", "x", "x", "x", "x"]
+      ],
+      canvas: { x: 5, y: 5 },
+      outputImage: ""
+    };
+
+    createTextCanvas({
+      state,
+      commit: (name, value) => {
+        updateOutputText(state, value);
+      }
+    });
+    expect(state.outputImage).toEqual(
+      "------\n" +
+        "|xxxxx|\n" +
+        "|xxxxx|\n" +
+        "|xxxxx|\n" +
+        "|xxxxx|\n" +
+        "|xxxxx|\n" +
+        "------\n"
+    );
+  });
+  it("test 2 creating text copy of canvas", () => {
+    const state = {
+      pixels: [
+        [
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "x",
+          "x",
+          "x",
+          "x",
+          "x"
+        ],
+        [
+          "x",
+          "x",
+          "x",
+          "x",
+          "x",
+          "x",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "x",
+          "",
+          "",
+          "",
+          "x"
+        ],
+        [
+          "",
+          "",
+          "",
+          "",
+          "",
+          "x",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "x",
+          "x",
+          "x",
+          "x",
+          "x"
+        ],
+        [
+          "",
+          "",
+          "",
+          "",
+          "",
+          "x",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o",
+          "o"
+        ]
+      ],
+      canvas: { x: 20, y: 4 },
+      outputImage: ""
+    };
+
+    createTextCanvas({
+      state,
+      commit: (name, value) => {
+        updateOutputText(state, value);
+      }
+    });
+    expect(state.outputImage).toEqual(
+      "---------------------\n" +
+        "|oooooooooooooooxxxxx|\n" +
+        "|xxxxxxooooooooox   x|\n" +
+        "|     xoooooooooxxxxx|\n" +
+        "|     xoooooooooooooo|\n" +
+        "---------------------\n"
+    );
+  });
+  it("test 3 creating text copy of canvas", () => {
+    const state = {
+      pixels: [
+        ["", "", "", "o", "o"],
+        ["", "o", "", "o", "o"],
+        ["", "", "", "o", "o"],
+        ["o", "o", "o", "o", "o"],
+        ["o", "o", "o", "o", "o"]
+      ],
+      canvas: { x: 5, y: 5 },
+      outputImage: ""
+    };
+
+    createTextCanvas({
+      state,
+      commit: (name, value) => {
+        updateOutputText(state, value);
+      }
+    });
+    expect(state.outputImage).toEqual(
+      "------\n" +
+        "|   oo|\n" +
+        "| o oo|\n" +
+        "|   oo|\n" +
+        "|ooooo|\n" +
+        "|ooooo|\n" +
+        "------\n"
+    );
+  });
+});
