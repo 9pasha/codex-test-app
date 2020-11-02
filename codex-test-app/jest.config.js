aec025fd78dca4e6bb3b98f7bbf4766fecee8747
Pavel Bolozhko <pavel.b820@gmail.com>
2020-11-02 13:17:15 +0300

    refactor: refactor jest.config

--- codex-test-app/jest.config.js
+++ codex-test-app/jest.config.js
@@ -1,42 +1,35 @@
-// module.exports = {
-//   preset: "@vue/cli-plugin-unit-jest"
-// };
 module.exports = {
   moduleFileExtensions: [
-    'js',
-    'jsx',
-    'json',
+    "js",
+    "jsx",
+    "json",
     // tell Jest to handle *.vue files
-    'vue'
+    "vue"
   ],
   transform: {
     // process *.vue files with vue-jest
-    '^.+\\.vue$': require.resolve('vue-jest'),
-    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
-        require.resolve('jest-transform-stub'),
-    '^.+\\.jsx?$': require.resolve('babel-jest'),
+    "^.+\\.vue$": require.resolve("vue-jest"),
+    ".+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$": require.resolve(
+      "jest-transform-stub"
+    ),
+    "^.+\\.jsx?$": require.resolve("babel-jest"),
     "\\.graphql$": require.resolve("jest-raw-loader"),
     "\\.md$": require.resolve("jest-raw-loader"),
     "\\.txt$": require.resolve("jest-raw-loader")
   },
-  transformIgnorePatterns: ['/node_modules/'],
+  transformIgnorePatterns: ["/node_modules/"],
   // support the same @ -> src alias mapping in source code
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
+    "^@/(.*)$": "<rootDir>/src/$1"
   },
-  testEnvironment: 'jest-environment-jsdom-fifteen',
+  testEnvironment: "jest-environment-jsdom-fifteen",
   // serializer for snapshots
-  snapshotSerializers: [
-    'jest-serializer-vue'
-  ],
-  testMatch: [
-    '**/tests/unit/**/*.spec.[jt]s?(x)',
-    '**/__tests__/*.[jt]s?(x)'
-  ],
+  snapshotSerializers: ["jest-serializer-vue"],
+  testMatch: ["**/tests/unit/**/*.spec.[jt]s?(x)", "**/__tests__/*.[jt]s?(x)"],
   // https://github.com/facebook/jest/issues/6766
-  testURL: 'http://localhost/',
+  testURL: "http://localhost/",
   watchPlugins: [
-    require.resolve('jest-watch-typeahead/filename'),
-    require.resolve('jest-watch-typeahead/testname')
+    require.resolve("jest-watch-typeahead/filename"),
+    require.resolve("jest-watch-typeahead/testname")
   ]
 };
